39e5a9c65fd1da5315d73594a16c56c51a794730
Clay Kohut <claykohut@MacBook-Pro-3.local>
2017-02-25 23:37:49 -0500

    got basic higher order component for redirect on auth

--- src/components/hoc/require_auth.js
+++ src/components/hoc/require_auth.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+
+    componentWillMount(){
+      if(!this.props.authenticated){
+        this.props.router.push('/')
+      }
+    }
+
+    componentWillUpdate(nextProps){
+      if(!nextProps.authenticated){
+        this.props.router.push('/')
+      }
+    }
+
     render() {
+      const { authenticated } = this.props
       return <ComposedComponent {...this.props} />
     }
   }
 
-  return Authentication
+  function mapStateToProps(state){
+    return {
+      authenticated: state.authenticated
+    }
+  }
+
+  return connect(mapStateToProps)(withRouter(Authentication))
 }
